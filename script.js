495247960cb994cc2c9d3a60392e1f5034baa496
kelvin358 <kelvinli358@gmail.com>
2020-01-29 02:16:14 +0000

    Character Quiz

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Write your click handler and conditionals here
+$("button").click(function(){
+    var height = $(".height").val();
+    var height = Number(height);
+    var position = $(".position").val();
+    
+if (height <= 160 && position === "libero") {
+    $("#result").html("Nishinoya");
+} 
+
+else if (height > 160 && position === "setter") {
+        $("#result").html("Kageyama");
+}
+
+else if (height > 180 && position === "wing spiker") {
+     $("#result").html("Bokuto");
+} 
+
+else if (height > 190 && position === "middle blocker") {
+        $("#result").html("Tsukishima");
+}
+
+else {
+        $("#result").html("Welcome to the Bench!!! Try Again!!!");
+}
+
+});
